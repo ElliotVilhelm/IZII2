cffc5c8934fde75d8ab2c36807b7937ab97d04e5
Elliot <elliot@pourmand.com>
2018-03-29 01:46:45 -0700

    woot woot first commit

---
+++ arrayToBitBoards.c
@@ -0,0 +1,17 @@
+/*
+ * File Name: arrayToBitBoards.c
+ * Author: Elliot Pourmand
+ * Description: This files purpose is to take an 8x8  char array and
+ *              generate the respective bitboards
+ */
+
+#include <stdio.h>
+#include "constants.c"
+
+void arrayToBitBoards(char board[][BOARD_SIZE],
+                      long * WP, long * WN, long * WB,
+                      long * WR, long * WQ, long * WK,
+                      long * BP, long * BN, long * BB,
+                      long * BR, long * BQ, long * BK) {
+    printf("arrayToBitBoards!");
+}
---
+++ testarrayToBitBoards.c
@@ -0,0 +1,31 @@
+/*
+ * File Name: testarrayToBitBoards.c
+ * Author: Elliot Pourmand
+ * Description: test file for arrayToBitBoards()
+ */
+#include <stdio.h>
+#include "constants.c"
+#include "arrayToBitBoards.c"
+
+void testarrayToBitBoards() {
+    long WP = 0L, WN = 0L, WB = 0L, WR = 0L, WQ = 0L, WK = 0L;
+    long BP = 0L, BN = 0L, BB = 0L, BR = 0L, BQ = 0L, BK = 0L;
+    char chessBoard[][BOARD_SIZE] = {
+            {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+    };
+    arrayToBitBoards();
+}
+
+int main() {
+    printf("Calling testarrayToBitBoards()\n");
+    testarrayToBitBoards();
+    printf("Finished testarrayToBitBoards()\n");
+}
+
